cdd88b90eb869b9ce3fee00a58904bb11caa3da3
Yael Gonzalez <123508058+yaelglz@users.noreply.github.com>
2023-04-11 12:39:29 -0600

    Inicio

---
+++ CF_1/HolaNictez/src/main/HolaNictez.java
@@ -0,0 +1,18 @@
+package main;
+
+public class HolaNictez {
+
+	public static void main(String[] args) {
+		// Este es una Hola Mundo
+		
+		/*
+		 * Este es un comentario
+		 * con saltos de linea
+		 * :D
+		 * */
+		
+		System.out.println("Hola Nictez!");
+		
+	}
+
+}
---
+++ CF_1/HolaNictez/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yaelc
+ *
+ */
+module HolaNictez {
+}
